cf05736593871742826820b039152e4f3abef548
parazitexe <chobak789>
2021-03-30 01:39:00 +0300

    2/3 partials ejs https://youtu.be/9EtkpCzqZu0?t=2444

--- index.js
+++ index.js
@@ -8,16 +8,24 @@ const __dirname = path.resolve();
 
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs')
+
 app.listen(port, ()=>{
     console.log(`started has been on port ${port}...`);
 });
 
+app.get("/", (req, res)=>{
+    //res.render("filin")
+    //res.send("filin")
+    res.render("index", {title: "My index"});
+})
+app.get("/filin", (req, res)=>{
+    //res.render("filin")
+    //res.send("filin")
+    res.render("index", {title: "My filin"});
+})
 
 app.use(requestTime);
 app.use(logger);
 app.use(express.static(path.resolve(__dirname, 'static')))
 
-
-/*app.get("/load", (req, res)=>{
-    res.download(path.resolve(__dirname, 'static','filin.html'));
-})*/
